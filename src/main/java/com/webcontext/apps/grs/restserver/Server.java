497c22a985624dfea4178e1649f774542cfe424c
Frédéric Delorme <frederic.delorme@gmail.com>
2014-08-16 02:40:06 +0200

    Add comments.

--- src/main/java/com/webcontext/apps/grs/restserver/Server.java
+++ src/main/java/com/webcontext/apps/grs/restserver/Server.java
@@ -16,7 +16,7 @@ import com.webcontext.apps.grs.service.DataManager;
  * This is the start class for our server.
  * 
  * @author Frédéric Delorme<frederic.delorme@web-context.com>
- *
+ * 
  */
 public class Server {
 
@@ -29,21 +29,29 @@ public class Server {
 		int port = 0;
 		String stopKey = "";
 		try {
+			// initialize server.
 			port = RestServer.getIntArg(args, "port", 8888);
 			stopKey = RestServer.getStringArg(args, "StopKey", "STOP");
 			server = new RestServer(port, stopKey);
+
+			// Add a new repository.
 			DataManager.getInstance()
 					.register(Game.class, GameRepository.class);
 
+			// add a new Handler to the Rest Server.
 			server.addRestContext("/rest/games", new GamesRestHandler(server));
 
+			// and start server.
 			server.start();
+
 		} catch (IOException | InterruptedException | InstantiationException
 				| IllegalAccessException e) {
 			LOGGER.error("Unable to start the internal Rest HTTP Server component on port "
 					+ port + ". Reason : " + e.getLocalizedMessage());
 		}
 		LOGGER.info("End of processing request in Server on port " + port);
+
+		// Exit from server.
 		System.exit(0);
 	}
 }
