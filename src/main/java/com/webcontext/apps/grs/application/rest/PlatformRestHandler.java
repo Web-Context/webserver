d4a566bbe47cf86080fa208e1c99dcc2e650ce23
Frédéric Delorme <frederic.delorme@gmail.com>
2014-08-19 03:59:03 +0200

    Add a RestRequest to parse JSON object.

---
+++ src/main/java/com/webcontext/apps/grs/application/rest/PlatformRestHandler.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.webcontext.apps.grs.application.rest;
+
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+
+import com.webcontext.apps.grs.framework.services.web.response.handler.impl.rest.RestHandler;
+import com.webcontext.apps.grs.framework.services.web.response.handler.impl.rest.RestResponse;
+import com.webcontext.apps.grs.framework.services.web.response.io.HttpRequest;
+import com.webcontext.apps.grs.framework.services.web.server.GenericServer;
+import com.webcontext.apps.grs.framework.services.web.server.GenericServer.HttpStatus;
+
+/**
+ * @author frederic
+ * 
+ */
+public class PlatformRestHandler extends RestHandler {
+
+	private static final Logger LOGGER = Logger
+			.getLogger(PlatformRestHandler.class);
+
+	/**
+	 * @param server
+	 */
+	public PlatformRestHandler(GenericServer server) {
+		super(server);
+	}
+
+	@Override
+	public HttpStatus get(HttpRequest request, RestResponse response)
+			throws IOException {
+	
+		return super.get(request, response);
+	}
+	
+	
+}
