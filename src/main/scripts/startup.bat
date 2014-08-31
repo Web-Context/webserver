6071b90a06be9e4de6ef41326867ae7fa49173b6
Frédéric Delorme <frederic.delorme@gmail.com>
2014-08-31 16:24:27 +0200

    split project to core and samples

--- src/main/scripts/startup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Start the WebServer
-java -jar ${project.artifactId}-${project.version}-full-jar-with-dependencies.jar \
-${mainClass} \
-port=8888 stopkey=stop dbembedded=true dbport=27017
-
-pause
- 
